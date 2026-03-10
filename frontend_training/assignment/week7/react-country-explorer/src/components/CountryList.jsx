e368aa1996982b5cd61d4368fafb1fd560af0da9
pranayd2126 <dandapranayreddy2118@gmail.com>
2026-03-10 23:19:35 +0530

    fix tailwind vite plugin

--- frontend_training/assignment/week7/react-country-explorer/src/components/CountryList.jsx
+++ frontend_training/assignment/week7/react-country-explorer/src/components/CountryList.jsx
@@ -1,10 +1,11 @@
+
 import CountryCard from "./CountryCard";
 
 function CountryList({ countries }) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-      {countries.map((count, idx) => (
-        <CountryCard key={idx} count={count} />
+      {countries.map((country, idx) => (
+        <CountryCard key={idx} country={country} />
       ))}
     </div>
   );
